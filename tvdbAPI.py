5f2d8c79e8fe0001499b7ddb998c990c55266b25
Kevin Riehl <kevinriehl@gmail.com>
2020-01-22 03:16:21 -0800

    Added Docstrings for all non private functions

--- tvdbAPI.py
+++ tvdbAPI.py
@@ -55,20 +55,23 @@ class TVDB:
         
         self.session = requests.Session()
 
-    def authorize(self):
-        r = self.session.post(self.config['loginURL'], json=self.config['authPayload'], headers=self.headers).json()
-        error = r.get('Error')
-        if error:
-            raise invalidCredentials
-        token = r.get('token')
-        self.headers['Authorization'] = f'Bearer {token}'
-        self.__authorized = True
-
     def getShow(self, name):
+        """ Gets basic info about the show with given 'name'.
+        
+        Arguments:
+            name {String} -- The name of the show you are searching for.
+        
+        Raises:
+            invalidInput -- Raises if a non string is entered for name.
+            showNotFound -- Raises if no show was found for the given name/alias.
+        
+        Returns:
+            dict -- Returns a dictionary containg basic data about the show.
+        """
         if type(name) is not str:
             raise invalidInput("You have entered an invalid name. Please try again.")
         if not self.__authorized:
-            self.authorize()
+            self._authorize()
         params = {
             "name": name
         }
@@ -79,11 +82,26 @@ class TVDB:
         return r
         
     def getEpisodes(self, name, accuracy = 0.8):
+        """ Gets a list of all the episodes for a given show.
+        
+        Arguments:
+            name {String} -- The name of the show being searched for.
+        
+        Keyword Arguments:
+            accuracy {float} -- If no show with title found, how accurate should a match to the alias be. (default: {0.8})
+        
+        Raises:
+            invalidInput -- Raises if a non string is inputed for name.
+            invalidShowID -- Raises if a show was not found.
+        
+        Returns:
+            list -- Returns a list of all the episodes for a given show.
+        """
         if type(name) is not str:
             raise invalidInput(
                 "You have entered an invalid name. Please try again.")
         if not self.__authorized:
-            self.authorize()
+            self._authorize()
         id = self._getShowID(name, accuracy)
         if id == -1:
             raise invalidShowID("Show was not found, please try again")
@@ -99,15 +117,44 @@ class TVDB:
         return episodes
 
     def getEpisodeName(self, name, seasonNum, epNum, accuracy = 0.8):
+        """ Gets an episode by its name, based on the show name, season number, and episode number, and
+            cleaned of any special characters so it can be used to name files without error.
+        
+        Arguments:
+            name {String} -- The name of the show being searched for.
+            seasonNum {integer} -- The season number which the episode is in.
+            epNum {integer} -- The episode number in the season.
+        
+        Keyword Arguments:
+            accuracy {float} -- If no show with title found, how accurate should a match to the alias be. (default: {0.8})
+        
+        Raises:
+            invalidInput -- Raises if a non string is inputed for name.
+            invalidShowID -- Raises if a show was not found.
+        
+        Returns:
+            String -- Returns the name of the episode searched for, cleaned of all special characters.
+        """
         if type(name) is not str or type(seasonNum) is not int or type(epNum) is not int or seasonNum < 0 or epNum < 1:
             raise invalidInput(
                 "You have entered an invalid name. Please try again.")
         if not self.__authorized:
-            self.authorize()
+            self._authorize()
         id = self._getShowID(name,accuracy)
         if id == -1:
             raise invalidShowID
         return self._getEpisodeName(id, seasonNum, epNum)
+
+
+    def _authorize(self):
+        r = self.session.post(
+            self.config['loginURL'], json=self.config['authPayload'], headers=self.headers).json()
+        error = r.get('Error')
+        if error:
+            raise invalidCredentials
+        token = r.get('token')
+        self.headers['Authorization'] = f'Bearer {token}'
+        self.__authorized = True
 
     def _getShowID(self, name, accuracy):
         params = {
